7b180ffed5fa62c9ac079916a886e3ada1da0de4
kemelbek <kemelbekirsaliev@gmail.com>
2018-12-07 16:53:43 +0600

    Minjust parse.py added all the columns

--- src/example/kg_address.py
+++ src/example/kg_address.py
@@ -6,14 +6,32 @@
     page = response.html
 
 
-  
-    name_ru = _gettext(page.xpath("//span[contains(text(),'2. П')]/../../following-sibling::td//text()"))
+
+    for i in range(len(page.xpath("//tbody/tr/td[@class='center'][2]//text()"))):
+        address_id = _gettext(page.xpath("//tbody/tr["+str(i)+"]/td[2]//text()"))
+        list_street_ky = _gettext(page.xpath("//tbody/tr["+str(i)+"]/td[3]/div[@class='list-street']/p//text()"))
+        street_type_ky = _gettext(page.xpath("//tbody/tr["+str(i)+"]/td[3]/div[@class='street-type']/span//text()"))
+        list_street_ru= _gettext(page.xpath("//tbody/tr["+str(i)+"]/td[4]/div[@class='list-street']/p//text()"))
+        street_type_ru= _gettext(page.xpath("//tbody/tr["+str(i)+"]/td[4]/div[@class='street-type']/span//text()"))
+        pr_list_street_ky = _gettext(page.xpath("//tbody/tr["+str(i)+"]/td[5]/div[@class='list-street']/p/text()"))
+        pr_street_type_ky = _gettext(page.xpath("//tbody/tr["+str(i)+"]/td[5]/div[@class='street-type']/span//text()"))
+        pr_list_street_ru = _gettext(page.xpath("//tbody/tr["+str(i)+"]/td[6]/div[@class='list-street']/p/text()"))
+        pr_street_type_ru = _gettext(page.xpath("//tbody/tr["+str(i)+"]/td[6]/div[@class='street-type']/span//text()"))
+
    
 
     org_data = {
     "url": response.url,
     
-    "name_ru": name_ru,
+    "address_id": address_id,
+    "list_street_ky":list_street_ky,
+    "street_type_ky":street_type_ky,
+    "list_street_ru":list_street_ru,
+    "street_type_ru":street_type_ru,
+    "pr_list_street_ky":pr_list_street_ky,
+    "pr_street_type_ky":pr_street_type_ky,
+    "pr_list_street_ru":pr_list_street_ru,
+    "pr_street_type_ru":pr_street_type_ru
     
     }
 
