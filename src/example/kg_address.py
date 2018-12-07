1a27cdc95e1de61f91c1b4a47b6426be9658c2e7
kemelbek <kemelbekirsaliev@gmail.com>
2018-12-07 17:04:54 +0600

    Indent typo

--- src/example/kg_address.py
+++ src/example/kg_address.py
@@ -7,42 +7,42 @@ def extractdata(context, data):
 
 
 
-for i in range(len(page.xpath("//tbody/tr/td[@class='center'][2]//text()"))):
-    address_id = _gettext(page.xpath("//tbody/tr["+str(i)+"]/td[2]//text()"))
-    list_street_ky = _gettext(page.xpath("//tbody/tr["+str(i)+"]/td[3]/div[@class='list-street']/p//text()"))
-    street_type_ky = _gettext(page.xpath("//tbody/tr["+str(i)+"]/td[3]/div[@class='street-type']/span//text()"))
-    list_street_ru= _gettext(page.xpath("//tbody/tr["+str(i)+"]/td[4]/div[@class='list-street']/p//text()"))
-    street_type_ru= _gettext(page.xpath("//tbody/tr["+str(i)+"]/td[4]/div[@class='street-type']/span//text()"))
-    pr_list_street_ky = _gettext(page.xpath("//tbody/tr["+str(i)+"]/td[5]/div[@class='list-street']/p/text()"))
-    pr_street_type_ky = _gettext(page.xpath("//tbody/tr["+str(i)+"]/td[5]/div[@class='street-type']/span//text()"))
-    pr_list_street_ru = _gettext(page.xpath("//tbody/tr["+str(i)+"]/td[6]/div[@class='list-street']/p/text()"))
-    pr_street_type_ru = _gettext(page.xpath("//tbody/tr["+str(i)+"]/td[6]/div[@class='street-type']/span//text()"))
+    for i in range(len(page.xpath("//tbody/tr/td[@class='center'][2]//text()"))):
+        address_id = _gettext(page.xpath("//tbody/tr["+str(i)+"]/td[2]//text()"))
+        list_street_ky = _gettext(page.xpath("//tbody/tr["+str(i)+"]/td[3]/div[@class='list-street']/p//text()"))
+        street_type_ky = _gettext(page.xpath("//tbody/tr["+str(i)+"]/td[3]/div[@class='street-type']/span//text()"))
+        list_street_ru= _gettext(page.xpath("//tbody/tr["+str(i)+"]/td[4]/div[@class='list-street']/p//text()"))
+        street_type_ru= _gettext(page.xpath("//tbody/tr["+str(i)+"]/td[4]/div[@class='street-type']/span//text()"))
+        pr_list_street_ky = _gettext(page.xpath("//tbody/tr["+str(i)+"]/td[5]/div[@class='list-street']/p/text()"))
+        pr_street_type_ky = _gettext(page.xpath("//tbody/tr["+str(i)+"]/td[5]/div[@class='street-type']/span//text()"))
+        pr_list_street_ru = _gettext(page.xpath("//tbody/tr["+str(i)+"]/td[6]/div[@class='list-street']/p/text()"))
+        pr_street_type_ru = _gettext(page.xpath("//tbody/tr["+str(i)+"]/td[6]/div[@class='street-type']/span//text()"))
 
-   
+       
 
-org_data = {
-"url": response.url,
-"address_id": address_id,
-"list_street_ky":list_street_ky,
-"street_type_ky":street_type_ky,
-"list_street_ru":list_street_ru,
-"street_type_ru":street_type_ru,
-"pr_list_street_ky":pr_list_street_ky,
-"pr_street_type_ky":pr_street_type_ky,
-"pr_list_street_ru":pr_list_street_ru,
-"pr_street_type_ru":pr_street_type_ru
+    org_data = {
+    "url": response.url,
+    "address_id": address_id,
+    "list_street_ky":list_street_ky,
+    "street_type_ky":street_type_ky,
+    "list_street_ru":list_street_ru,
+    "street_type_ru":street_type_ru,
+    "pr_list_street_ky":pr_list_street_ky,
+    "pr_street_type_ky":pr_street_type_ky,
+    "pr_list_street_ru":pr_list_street_ru,
+    "pr_street_type_ru":pr_street_type_ru
 
-}
+    }
 
-print("----------------Printing Org Data------------------")
-print(org_data)
+    print("----------------Printing Org Data------------------")
+    print(org_data)
 
-context.emit(data=org_data)
+    context.emit(data=org_data)
 
-    
+        
 
-def _gettext(list):
-    if not list:
-        return list
-    else:
-        return list[0].strip()
+    def _gettext(list):
+        if not list:
+            return list
+        else:
+            return list[0].strip()
